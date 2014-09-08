ff79ab40f2ba08d2d73b1889ef6566c4e71752a7
gordon <wgordonw1@gmail.com>
2014-09-08 14:30:42 -0400

    Revert "fixes https://github.com/torchbox/wagtail/issues/564"
    This reverts commit 2d5752b6ea3b3b77d4a006d1395532a1c1414dc4.

--- wagtail/wagtailimages/models.py
+++ wagtail/wagtailimages/models.py
@@ -5,7 +5,6 @@ from six import BytesIO
 
 from taggit.managers import TaggableManager
 
-import django
 from django.core.files import File
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.db import models
@@ -214,9 +213,7 @@ class AbstractImage(models.Model, TagSearchable):
 
 
 class Image(AbstractImage):
-    if not django.VERSION < (1, 7):
-        class Meta(AbstractImage.Meta):
-            swappable = 'WAGTAILIMAGES_IMAGE_MODEL'
+    pass
 
 
 # Do smartcropping calculations when user saves an image without a focal point
@@ -372,7 +369,7 @@ class AbstractRendition(models.Model):
 
 
 class Rendition(AbstractRendition):
-    image = models.ForeignKey(getattr(settings, "WAGTAILIMAGES_IMAGE_MODEL", Image), related_name='renditions')
+    image = models.ForeignKey('Image', related_name='renditions')
 
     class Meta:
         unique_together = (
